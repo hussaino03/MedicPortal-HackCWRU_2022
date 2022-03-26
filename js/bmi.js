bce61523de3381ba19eba216c92780e8461d4cee
hussaino03 <m.hussainomer03@gmail.com>
2022-03-25 22:41:19 -0400

    Update bmi.js

--- js/bmi.js
+++ js/bmi.js
@@ -32,10 +32,10 @@ $(window).scroll(function () {
     // Checking the user providing a proper
     // value or not
     if (height === "" || isNaN(height))
-      result.innerHTML = "Provide a valid Height!";
+      result.innerHTML = "Please provide a valid Height!";
   
     else if (weight === "" || isNaN(weight))
-      result.innerHTML = "Provide a valid Weight!";
+      result.innerHTML = "Please provide a valid Weight!";
   
     // If both input is valid, calculate the bmi
     else {
